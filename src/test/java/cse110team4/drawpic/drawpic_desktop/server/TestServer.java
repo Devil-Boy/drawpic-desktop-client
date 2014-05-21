d1e5d876b52436ddfce07919f31a5158d92b4e80
Kirk <kkw013@ucsd.edu>
2014-05-20 22:58:21 -0700

    Added tests for the Server interface

--- src/test/java/cse110team4/drawpic/drawpic_desktop/server/TestServer.java
+++ src/test/java/cse110team4/drawpic/drawpic_desktop/server/TestServer.java
@@ -4,11 +4,41 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * This class will test the server interface. Does not test specific implementation
+ * @author Kirk
+ *
+ */
 public class TestServer {
-
+	
+	Server server;
+	
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	public void testLogin() {
+		assertNull(server.login("Bob"));
+		assertNotNull(server.login("Bob")); //adding the same name
+		assertNull(server.login(" "));
+	}
+	
+	@Test
+	public void testCreateLobby(){
+		assertNotNull(server.createLobby());
+	}
+	
+	@Test
+	public void testOpenLobbies(){
+		assertNotNull(server.openLobbies());
+	}
+	
+	@Test
+	public void testGetLobby(){
+		assertNotNull(server.getLobby("Bob"));
+	}
+	
+	@Test
+	public void testJoinLobby(){
+		server.login("Steve");
+		assertNotNull(server.joinLobby((server.getLobby("Bob"))));
 	}
 
 }
