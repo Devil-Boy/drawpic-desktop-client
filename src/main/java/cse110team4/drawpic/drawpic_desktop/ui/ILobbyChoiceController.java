e7af90351692f326a11467128d32def8178942e1
Kirk <kkw013@ucsd.edu>
2014-06-03 05:14:32 -0700

    Created LobbyChoice Interfaces

---
+++ src/main/java/cse110team4/drawpic/drawpic_desktop/ui/ILobbyChoiceController.java
@@ -0,0 +1,12 @@
+package cse110team4.drawpic.drawpic_desktop.ui;
+
+/**
+ * @author Kirk
+ *
+ */
+public interface ILobbyChoiceController {
+
+	public void LobbyOption();
+	
+	public void setView(ILobbyChoiceView viewer);
+}
