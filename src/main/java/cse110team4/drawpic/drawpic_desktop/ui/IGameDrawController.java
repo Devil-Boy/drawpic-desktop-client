755fef33eb1a78b9cbb962979d03d6f681028a55
Devil Boy <devilboy@pinoygamers.info>
2014-06-04 15:52:55 -0700

    Added a semicolon

--- src/main/java/cse110team4/drawpic/drawpic_desktop/ui/IGameDrawController.java
+++ src/main/java/cse110team4/drawpic/drawpic_desktop/ui/IGameDrawController.java
@@ -8,7 +8,7 @@ public interface IGameDrawController {
 
 	public void addStep(Step step);
 	
-	public void changeToPen(Color color, int size)
+	public void changeToPen(Color color, int size);
 	
 	public void erase();
 }
