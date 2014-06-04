d19b34269f79922641ecf0efd0e1881d8db893d4
mcdirect <mcdirect@ucsd.edu>
2014-06-04 02:08:59 -0700

    UnKirked

---
+++ src/main/java/cse110team4/drawpic/drawpic_desktop/ui/IInLobbyController.java
@@ -0,0 +1,31 @@
+package cse110team4.drawpic.drawpic_desktop.ui;
+
+import cse110team4.drawpic.drawpic_core.player.Lobby;
+import cse110team4.drawpic.drawpic_core.player.NormalLobbySettings.JudgeSetting;
+
+
+public interface IInLobbyController {
+
+	//Lobby is the lobby 
+	public void leaveLobby();
+	
+	//Lobby is the lobby whose game we're starting
+	//returns true if game can be started (4 player requirement)
+	//else returns false
+	public boolean startGame(Lobby lobby);
+	
+	//JudgeSetting can only be from the JudgeSetting enum
+	//(RANDOM, HOST_ONLY, IN_ORDER)
+	public void changeSettingJudgeMode(JudgeSetting setting);
+	
+	//num is number of rounds per game
+	//returns true if valid number of rounds (not less than number of wins)
+	public boolean changeSettingNumRounds(int num);
+	
+	//num is the number of wins for a player to win the game
+	//returns true if valid (not more than number of rounds)
+	public boolean changeSettingNumWins(int num);
+	
+	//time is the time per draw phase (in seconds)
+	public void changeSettingDrawTime(int time);
+}
---
+++ src/main/java/cse110team4/drawpic/drawpic_desktop/ui/InLobbyController.java
@@ -0,0 +1,56 @@
+package cse110team4.drawpic.drawpic_desktop.ui;
+
+import javax.swing.JOptionPane;
+
+import cse110team4.drawpic.drawpic_core.player.Lobby;
+import cse110team4.drawpic.drawpic_core.player.NormalLobbySettings.JudgeSetting;
+import cse110team4.drawpic.drawpic_desktop.DesktopBeans;
+import cse110team4.drawpic.drawpic_desktop.server.ServerConnection;
+
+public class InLobbyController implements IInLobbyController {
+
+	IInLobbyView view;
+	
+	ServerConnection connection;
+	
+	public InLobbyController(ServerConnection connection, IInLobbyView view) {
+		this.connection = connection;
+		this.view = view;
+	}
+	
+	@Override
+	public void leaveLobby() {
+		String leaveResult = connection.leaveLobby();
+		if (leaveResult == null) {
+			DesktopBeans.getContext().getBean(DrawPicApp.class).setCurrentPhase(ClientPhase.LOBBY_BROWSING);
+		} else {
+			JOptionPane.showMessageDialog(null, "Error with leaving lobby:\n" + leaveResult);
+		}
+	}
+
+	@Override
+	public boolean startGame(Lobby lobby) {
+		return false;
+	}
+
+	@Override
+	public void changeSettingJudgeMode(JudgeSetting setting) {
+
+	}
+
+	@Override
+	public boolean changeSettingNumRounds(int num) {
+		return false;
+	}
+
+	@Override
+	public boolean changeSettingNumWins(int num) {
+		return false;
+	}
+
+	@Override
+	public void changeSettingDrawTime(int time) {
+
+	}
+
+}
