f4a1adeed7858935ec1f5312978432dd73bfcc0d
Kirk <kkw013@ucsd.edu>
2014-06-04 10:37:09 -0700

    Added UI for drawing

---
+++ src/main/java/cse110team4/drawpic/drawpic_desktop/ui/swing/panel/GameDrawView.java
@@ -0,0 +1,86 @@
+package cse110team4.drawpic.drawpic_desktop.ui.swing.panel;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Label;
+import javax.swing.JMenuBar;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import javax.swing.JColorChooser;
+import java.awt.ScrollPane;
+import javax.swing.JSeparator;
+import java.awt.GridLayout;
+import javax.swing.JSlider;
+import javax.swing.JRadioButton;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ButtonGroup;
+
+public class GameDrawView extends JPanel {
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+
+	/**
+	 * Create the panel.
+	 */
+	public GameDrawView() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.CENTER);
+		
+		Canvas canvas = new Canvas();
+		panel.add(canvas);
+		
+		Label label = new Label("New label");
+		label.setAlignment(Label.CENTER);
+		add(label, BorderLayout.NORTH);
+		
+		JPanel panel_1 = new JPanel();
+		add(panel_1, BorderLayout.EAST);
+		panel_1.setLayout(new FormLayout(new ColumnSpec[] {
+				ColumnSpec.decode("241px"),},
+			new RowSpec[] {
+				RowSpec.decode("75px"),
+				RowSpec.decode("201px"),}));
+		
+		JPanel panel_3 = new JPanel();
+		panel_1.add(panel_3, "1, 1, fill, fill");
+		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel_4 = new JPanel();
+		panel_3.add(panel_4);
+		
+		JRadioButtonMenuItem radioButtonMenuItem = new JRadioButtonMenuItem("Drawing Tools:");
+		panel_4.add(radioButtonMenuItem);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Pen");
+		panel_4.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnEraser = new JRadioButton("Eraser");
+		panel_4.add(rdbtnEraser);
+		//panel_4.add(rdbtnEraser);
+		
+		JPanel panel_2 = new JPanel();
+		panel_3.add(panel_2);
+		
+		JLabel lblNewLabel = new JLabel("Size:");
+		panel_2.add(lblNewLabel);
+		
+		JSlider slider = new JSlider();
+		panel_2.add(slider);
+		
+		JColorChooser colorChooser = new JColorChooser();
+		//panel_1.add(colorChooser);
+		
+		ScrollPane scrollPane = new ScrollPane();
+		panel_1.add(scrollPane, "1, 2, fill, fill");
+		scrollPane.add(colorChooser);
+
+	}
+
+}
