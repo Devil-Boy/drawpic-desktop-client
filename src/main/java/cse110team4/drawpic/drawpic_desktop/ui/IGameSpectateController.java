196a7752acf2b5e6153c3c78e1a9b8e775167f73
Kirk <kkw013@ucsd.edu>
2014-06-04 06:52:43 -0700

    created interfaces for judge

---
+++ src/main/java/cse110team4/drawpic/drawpic_desktop/ui/IGameSpectateController.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package cse110team4.drawpic.drawpic_desktop.ui;
+
+/**
+ * Interface to match for judges. Will cycle through canvases at the push of a button. 
+ * @author Kirk
+ *
+ */
+public interface IGameSpectateController {
+
+	public void cycleCanvas();
+}
---
+++ src/main/java/cse110team4/drawpic/drawpic_desktop/ui/IGameSpectateView.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package cse110team4.drawpic.drawpic_desktop.ui;
+
+/**
+ * @author Kirk
+ *
+ */
+public interface IGameSpectateView {
+
+	public void settController();
+}
