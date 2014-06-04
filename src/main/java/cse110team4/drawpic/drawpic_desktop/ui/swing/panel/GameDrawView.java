cfaf887384b499d809cab2d4e0a67fe6f74a91a1
Kirk <kkw013@ucsd.edu>
2014-06-04 10:57:42 -0700

    Updated drawing UI

--- src/main/java/cse110team4/drawpic/drawpic_desktop/ui/swing/panel/GameDrawView.java
+++ src/main/java/cse110team4/drawpic/drawpic_desktop/ui/swing/panel/GameDrawView.java
@@ -20,6 +20,7 @@ import java.awt.FlowLayout;
 import java.awt.CardLayout;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.ButtonGroup;
+import javax.swing.border.EtchedBorder;
 
 public class GameDrawView extends JPanel {
 	private final ButtonGroup buttonGroup = new ButtonGroup();
@@ -31,6 +32,7 @@ public class GameDrawView extends JPanel {
 		setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		add(panel, BorderLayout.CENTER);
 		
 		Canvas canvas = new Canvas();
@@ -43,27 +45,25 @@ public class GameDrawView extends JPanel {
 		JPanel panel_1 = new JPanel();
 		add(panel_1, BorderLayout.EAST);
 		panel_1.setLayout(new FormLayout(new ColumnSpec[] {
-				ColumnSpec.decode("241px"),},
+				ColumnSpec.decode("234px"),},
 			new RowSpec[] {
 				RowSpec.decode("75px"),
 				RowSpec.decode("201px"),}));
 		
 		JPanel panel_3 = new JPanel();
-		panel_1.add(panel_3, "1, 1, fill, fill");
+		panel_1.add(panel_3, "1, 1, left, fill");
 		panel_3.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JPanel panel_4 = new JPanel();
 		panel_3.add(panel_4);
 		
-		JRadioButtonMenuItem radioButtonMenuItem = new JRadioButtonMenuItem("Drawing Tools:");
-		panel_4.add(radioButtonMenuItem);
-		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Pen");
+		buttonGroup.add(rdbtnNewRadioButton);
 		panel_4.add(rdbtnNewRadioButton);
 		
 		JRadioButton rdbtnEraser = new JRadioButton("Eraser");
+		buttonGroup.add(rdbtnEraser);
 		panel_4.add(rdbtnEraser);
-		//panel_4.add(rdbtnEraser);
 		
 		JPanel panel_2 = new JPanel();
 		panel_3.add(panel_2);
